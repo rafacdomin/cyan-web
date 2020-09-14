fa262619f5d75a138550ce08f0fe7e2cec71bb64
Rafael <rafaelcodomingues@gmail.com>
2020-09-14 17:58:51 -0300

    :tada: Register page

--- src/pages/Register/index.tsx
+++ src/pages/Register/index.tsx
@@ -1,9 +1,79 @@
-import React from 'react';
+import React, { useCallback, useRef } from 'react';
+import { Form } from '@unform/web';
+import { FormHandles } from '@unform/core';
+import { FiMail, FiLock, FiUser } from 'react-icons/fi';
+import * as Yup from 'yup';
 
-// import { Container } from './styles';
+import getValidationErrors from '../../utils/getValidationErrors';
+import Input from '../../components/Input';
+import cyan from '../../assets/cyan.svg';
+import { Container } from './styles';
+
+interface FormData {
+  name: string;
+  email: string;
+  password: string;
+  passwordConfirm: string;
+}
 
 const Register: React.FC = () => {
-  return <div />;
+  const formRef = useRef<FormHandles>(null);
+
+  const handleSubmit = useCallback(async (data: FormData) => {
+    try {
+      formRef.current?.setErrors({});
+
+      const schema = Yup.object().shape({
+        name: Yup.string()
+          .min(3, 'Name must be at least 3 characters')
+          .required('Name is required'),
+        email: Yup.string().email().required('Email is required'),
+        password: Yup.string()
+          .min(6, 'Password must be at least 6 characters')
+          .required('Password is required'),
+        passwordConfirm: Yup.string()
+          .oneOf([Yup.ref('password')], 'Passwords must match')
+          .required('Passwords must match'),
+      });
+
+      await schema.validate(data, {
+        abortEarly: false,
+      });
+    } catch (err) {
+      if (err instanceof Yup.ValidationError) {
+        const errors = getValidationErrors(err);
+
+        formRef.current?.setErrors(errors);
+
+        return;
+      }
+    }
+  }, []);
+
+  return (
+    <Container>
+      <img src={cyan} alt="cyan agroanalytics" />
+
+      <Form ref={formRef} onSubmit={handleSubmit}>
+        <Input name="name" type="text" placeholder="Name" icon={FiUser} />
+        <Input name="email" type="email" placeholder="E-mail" icon={FiMail} />
+        <Input
+          name="password"
+          type="password"
+          placeholder="Password"
+          icon={FiLock}
+        />
+        <Input
+          name="passwordConfirm"
+          type="password"
+          placeholder="Confirm your password"
+          icon={FiLock}
+        />
+
+        <button>Register</button>
+      </Form>
+    </Container>
+  );
 };
 
 export default Register;
---
+++ src/pages/Register/styles.ts
@@ -0,0 +1,55 @@
+import styled from 'styled-components';
+import colors from '../../styles/colors';
+import { shade } from 'polished';
+
+export const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  padding: 6rem 0;
+
+  form {
+    margin-top: 3.2rem;
+    width: 36rem;
+    height: 40rem;
+
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+
+    border-radius: 0.4rem;
+    border: 0.05rem solid ${colors.primary};
+    box-shadow: 4px 4px 4px rgba(0, 0, 0, 0.25);
+
+    a {
+      align-self: flex-end;
+      margin-right: 2.8rem;
+      margin-top: 0.8rem;
+      text-decoration: none;
+      color: ${colors.primaryLighten};
+      font-size: 1.2rem;
+      transition: color 0.2s;
+
+      &:hover {
+        color: ${colors.primary};
+      }
+    }
+
+    button {
+      height: 5rem;
+      width: 30rem;
+      margin-top: 2.4rem;
+      background: ${colors.primary};
+      border: 0;
+      border-radius: 0.5rem;
+      color: ${colors.secondary};
+      transition: background-color 0.2s;
+
+      &:hover {
+        background: ${shade(0.08, colors.primary)};
+      }
+    }
+  }
+`;
